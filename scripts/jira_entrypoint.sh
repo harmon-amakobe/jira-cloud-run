d95f067b6c85d083056ad02a575db0ff793d33de
Harmon Amakobe <harmon@butsotso.com>
2023-07-25 14:15:58 -0400

    initial attempt at solution

---
+++ scripts/jira_entrypoint.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Create mount directory for JIRA_HOME
+mkdir -p $JIRA_SHARED_HOME
+
+echo "Mounting GCS Fuse for JIRA_HOME."
+gcsfuse --debug_gcs --debug_fuse $JIRA_HOME_BUCKET $JIRA_SHARED_HOME
+echo "Mounting completed."
+
+# Set up clustering configuration properties
+CLUSTERED="true"
+JIRA_NODE_ID="${JIRA_NODE_ID:-jira_node_}"
+EHCACHE_PEER_DISCOVERY="${EHCACHE_PEER_DISCOVERY:-default}"
+EHCACHE_LISTENER_HOSTNAME="${EHCACHE_LISTENER_HOSTNAME:-NONE}"
+EHCACHE_LISTENER_PORT="${EHCACHE_LISTENER_PORT:-40001}"
+EHCACHE_OBJECT_PORT="${EHCACHE_OBJECT_PORT:-dynamic}"
+EHCACHE_LISTENER_SOCKETTIMEOUTMILLIS="${EHCACHE_LISTENER_SOCKETTIMEOUTMILLIS:-2000}"
+EHCACHE_MULTICAST_ADDRESS="${EHCACHE_MULTICAST_ADDRESS:-NONE}"
+EHCACHE_MULTICAST_PORT="${EHCACHE_MULTICAST_PORT:-NONE}"
+EHCACHE_MULTICAST_TIMETOLIVE="${EHCACHE_MULTICAST_TIMETOLIVE:-NONE}"
+EHCACHE_MULTICAST_HOSTNAME="${EHCACHE_MULTICAST_HOSTNAME:-NONE}"
+
+# Start Jira Data Center with the shared home volume and clustering configuration
+exec /opt/atlassian/jira/bin/start-jira.sh -fg \
+    -Datlassian.plugins.enable.wait=300 \
+    -Datlassian.clustered="$CLUSTERED" \
+    -Datlassian.node.id="$JIRA_NODE_ID" \
+    -Datlassian.shared.home="$JIRA_SHARED_HOME" \
+    -Datlassian.cluster.ehcache.peer.discovery="$EHCACHE_PEER_DISCOVERY" \
+    -Datlassian.cluster.ehcache.listener.hostname="$EHCACHE_LISTENER_HOSTNAME" \
+    -Datlassian.cluster.ehcache.listener.port="$EHCACHE_LISTENER_PORT" \
+    -Datlassian.cluster.ehcache.object.port="$EHCACHE_OBJECT_PORT" \
+    -Datlassian.cluster.ehcache.listener.sockettimeoutmillis="$EHCACHE_LISTENER_SOCKETTIMEOUTMILLIS" \
+    -Datlassian.cluster.ehcache.multicast.address="$EHCACHE_MULTICAST_ADDRESS" \
+    -Datlassian.cluster.ehcache.multicast.port="$EHCACHE_MULTICAST_PORT" \
+    -Datlassian.cluster.ehcache.multicast.timetolive="$EHCACHE_MULTICAST_TIMETOLIVE" \
+    -Datlassian.cluster.ehcache.multicast.hostname="$EHCACHE_MULTICAST_HOSTNAME" \
+    "your-jira-flags"
