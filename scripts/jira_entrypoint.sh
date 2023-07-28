de69130d550172d2320a320e51381bcf75d53f4d
Harmon Amakobe <harmon@butsotso.com>
2023-07-28 03:19:14 -0400

    added logfile

--- scripts/jira_entrypoint.sh
+++ scripts/jira_entrypoint.sh
@@ -4,6 +4,11 @@ set -eo pipefail
 # Create mount directory for JIRA_HOME
 mkdir -p $JIRA_SHARED_HOME
 
+# Redirect output to a log file
+LOG_FILE="/app/jira_entrypoint.log"
+exec > >(tee -a ${LOG_FILE} )
+exec 2> >(tee -a ${LOG_FILE} >&2)
+
 # Print the value of JIRA_SHARED_HOME and _JIRA_HOME_BUCKET for debugging purposes
 echo "JIRA_SHARED_HOME: $JIRA_SHARED_HOME"
 echo "_JIRA_HOME_BUCKET: $_JIRA_HOME_BUCKET"
@@ -17,3 +22,6 @@ exec /opt/atlassian/jira/bin/start-jira.sh -fg \
     -Datlassian.plugins.enable.wait=300 \
     -Datlassian.clustered="true" \
     -Datlassian.shared.home="$JIRA_SHARED_HOME"
+
+# Output the log file at the end of the script execution
+cat ${LOG_FILE}
